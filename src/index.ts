9b70440b62b5985abbbf3f14f211ad62265d9365
matt <github.27jtz@aleeas.com>
2021-11-09 15:33:06 -0300

    docs: explain why lgtcr is commended out

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import gtcrBot from "./gtcr";
+
+// Light curate is the process of being deprecated in favor slot curate.
 // import lightGTCRExecuteBot from "./light-gtcr-execution";
 // import lightGTCRWithdrawBot from "./light-gtcr-withdrawal";
 
