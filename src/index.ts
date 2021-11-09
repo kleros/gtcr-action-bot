2f7560cf7e63b3721b38d573c09c2a71c35ee00a
matt <github.27jtz@aleeas.com>
2021-11-09 14:36:46 -0300

    chore: disable light curate

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import gtcrBot from "./gtcr";
-import lightGTCRExecuteBot from "./light-gtcr-execution";
-import lightGTCRWithdrawBot from "./light-gtcr-withdrawal";
+// import lightGTCRExecuteBot from "./light-gtcr-execution";
+// import lightGTCRWithdrawBot from "./light-gtcr-withdrawal";
 
 gtcrBot();
-lightGTCRWithdrawBot();
-lightGTCRExecuteBot();
+// lightGTCRWithdrawBot();
+// lightGTCRExecuteBot();
